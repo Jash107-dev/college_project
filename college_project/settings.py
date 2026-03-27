e66fc63148154b854dee3f472bba536e171bf96f
Jashwanth <mca111724039107@gmail.com>
2026-03-27 14:41:52 +0530

    postgre SQL

--- college_project/settings.py
+++ college_project/settings.py
@@ -38,13 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
     'accounts',
-<<<<<<< HEAD
     'inventory_master',
-=======
     'employee',
->>>>>>> 7cd3a45b25825b36ab3a99b0e038be447519076e
 ]
 
 MIDDLEWARE = [
