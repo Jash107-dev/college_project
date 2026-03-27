a61b3af2d880006839f58acca4fb0e0f8bb04bca
Jashwanth <mca111724039107@gmail.com>
2026-03-27 14:30:34 +0530

    postgre SQL

--- college_project/settings.py
+++ college_project/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'college_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'college_db',
+        'USER': 'postgres',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
